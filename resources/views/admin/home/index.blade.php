109f69e65ab722fbc621ee8148cdea84f8468ddb
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-05-04 13:22:05 +0500

    Add frontend assets & admin.home page

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1>admin home page</h1>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name') . '.admin' }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('admin.home') }}">
+                {{ config('app.name') . '.admin' }}
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('admin.home') }}">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="container py-4">
+        @yield('content')
+    </main>
+</body>
+</html>
