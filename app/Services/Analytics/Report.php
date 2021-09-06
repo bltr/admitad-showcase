704d18b28d06f19a80fefa82cdd8a00faf04eac9
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-09-06 20:18:56 +0500

    wip

--- app/Services/Analytics/Report.php
+++ app/Services/Analytics/Report.php
@@ -6,6 +6,8 @@ namespace App\Services\Analytics;
 
 interface Report
 {
+    public function __construct(string $base_view);
+
     public function build(int $shopId);
 
     public function render(): string;
--- app/Services/Feed/Analytics/CompositeReportFactory.php
+++ app/Services/Feed/Analytics/CompositeReportFactory.php
@@ -11,11 +11,18 @@ class CompositeReportFactory
 {
     private $base_view =  'admin.feeds.analytics';
 
+    private $reports = [
+        OffersCountReport::class,
+        CountGroupsReport::class,
+    ];
+
     public function build(): Report
     {
         $composite = new CompositeReport($this->base_view);
-        $composite->addReport(new OffersCountReport($this->base_view));
-        $composite->addReport(new CountGroupsReport($this->base_view));
+        foreach ($this->reports as $reportClassName) {
+            $report = new $reportClassName($this->base_view);
+            $composite->addReport($report);
+        };
 
         return $composite;
     }
