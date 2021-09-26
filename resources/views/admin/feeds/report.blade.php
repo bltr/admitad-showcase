b0e42bfa312598a99c1c57428ff12c2cda2e4f08
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-09-26 19:45:08 +0500

    wip

--- resources/views/admin/feeds/report.blade.php
+++ resources/views/admin/feeds/report.blade.php
@@ -11,39 +11,6 @@
     </div>
     <div class="row">
         <div class="col">
-            @if($report)
-                <div class="col-4 mb-4">
-                    <form action="{{ route('admin.feeds.import-type', $shop) }}" method="POST">
-                        @method('patch')
-                        @csrf
-
-                        <select name='import_type' class="form-select" onchange="this.form.submit()">
-                            <option value="">-</option>
-                            <option @if($shop->isImportWithoutGrouping()) selected @endif
-                                value="{{ $shop::IMPORT_WITHOUT_GROUPING }}"
-                            >
-                                {{ $shop::IMPORT_WITHOUT_GROUPING }}
-                            </option>
-                            <option @if($shop->isImportGroupByGroupId()) selected @endif
-                                value="{{ $shop::IMPORT_GROUP_BY_GROUP_ID }}"
-                            >
-                                {{ $shop::IMPORT_GROUP_BY_GROUP_ID }}
-                            </option>
-                            <option @if($shop->isImportGroupByPicture()) selected @endif
-                                value="{{ $shop::IMPORT_GROUP_BY_PICTURE }}"
-                            >
-                                {{ $shop::IMPORT_GROUP_BY_PICTURE }}
-                            </option>
-                            <option @if($shop->isImportGroupByUrl()) selected @endif
-                                value="{{ $shop::IMPORT_GROUP_BY_URL }}"
-                            >
-                                {{ $shop::IMPORT_GROUP_BY_URL }}
-                            </option>
-                        </select>
-                    </form>
-                </div>
-            @endif
-
             @include('admin.report.composite', compact('report'))
         </div>
     </div>
--- resources/views/admin/report/feed/groups_count.blade.php
+++ resources/views/admin/report/feed/groups_count.blade.php
@@ -76,5 +76,35 @@
                 </td>
             </tr>
         </table>
+
+        <form action="{{ route('admin.feeds.import-type', $shop) }}" method="POST">
+            @method('patch')
+            @csrf
+
+            <label for="import_type" class="form-label">Тип импорта</label>
+            <select name='import_type' id="import_type" class="form-select" onchange="this.form.submit()">
+                <option value="">-</option>
+                <option @if($shop->isImportWithoutGrouping()) selected @endif
+                value="{{ $shop::IMPORT_WITHOUT_GROUPING }}"
+                >
+                    {{ $shop::IMPORT_WITHOUT_GROUPING }}
+                </option>
+                <option @if($shop->isImportGroupByGroupId()) selected @endif
+                value="{{ $shop::IMPORT_GROUP_BY_GROUP_ID }}"
+                >
+                    {{ $shop::IMPORT_GROUP_BY_GROUP_ID }}
+                </option>
+                <option @if($shop->isImportGroupByPicture()) selected @endif
+                value="{{ $shop::IMPORT_GROUP_BY_PICTURE }}"
+                >
+                    {{ $shop::IMPORT_GROUP_BY_PICTURE }}
+                </option>
+                <option @if($shop->isImportGroupByUrl()) selected @endif
+                value="{{ $shop::IMPORT_GROUP_BY_URL }}"
+                >
+                    {{ $shop::IMPORT_GROUP_BY_URL }}
+                </option>
+            </select>
+        </form>
     </div>
 </div>
