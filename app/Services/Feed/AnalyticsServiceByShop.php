b309255de0e32765af1b87057bb8f046815f5e1d
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-09-26 15:45:01 +0500

    wip

--- app/Services/Feed/AnalyticsServiceByShop.php
+++ app/Services/Feed/AnalyticsServiceByShop.php
@@ -1,14 +1,13 @@
 <?php
 
-
 namespace App\Services\Feed;
 
 
 use App\Models\Analytics;
-use App\Models\Shop;
 use App\Services\Analytics\CompositeReport;
 use App\Services\Feed\AnalyticsReports\GroupsCountReport;
 use App\Services\Feed\AnalyticsReports\OffersCountReport;
+use App\Models\Shop;
 
 class AnalyticsServiceByShop
 {
