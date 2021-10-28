51a01b9f3d9f81cd1de0fc9f991cf022125bfe5e
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-10-28 18:45:01 +0500

    wip

--- resources/views/components/tree.blade.php
+++ resources/views/components/tree.blade.php
@@ -1,36 +1,40 @@
 @php $depth = $depth ?? 0; @endphp
-<ul class="{{ !$depth ? 'tree' : '' }} {{ $depth ? 'collapse' : '' }}"
-    @if($depth) id="{{ $id }}-collapse-{{ $parentItemId }}" @endif
->
-    @foreach($items as $item)
-        <li data-item-id="{{ $item->id }}">
-            <div class="tree_node">
-                <div class="tree_node_toggle">
-                    @if($item->children->isNotEmpty())
-                        <button type="button" class="btn" data-bs-toggle="collapse" data-bs-target="#{{ $id }}-collapse-{{ $item->id }}">
-                            <i class="bi bi-plus"></i>
-                        </button>
-                    @else
-                        <button type="button" class="btn disabled">
-                            <i class="bi bi-dot"></i>
-                        </button>
+@if($items->isNotEmpty())
+    <ul class="{{ !$depth ? 'tree' : '' }} {{ $depth ? 'collapse' : '' }}"
+        @if($depth) id="{{ $id }}-collapse-{{ $parentItemId }}" @endif
+    >
+        @foreach($items as $item)
+            <li data-item-id="{{ $item->id }}">
+                <div class="tree_node">
+                    <div class="tree_node_toggle">
+                        @if($item->children->isNotEmpty())
+                            <button type="button" class="btn" data-bs-toggle="collapse" data-bs-target="#{{ $id }}-collapse-{{ $item->id }}">
+                                <i class="bi bi-plus"></i>
+                            </button>
+                        @else
+                            <button type="button" class="btn disabled">
+                                <i class="bi bi-dot"></i>
+                            </button>
+                        @endif
+                    </div>
+
+                    @if(!empty($itemTemplate))
+                        @include($itemTemplate)
                     @endif
                 </div>
 
-                @if(!empty($itemTemplate))
-                    @include($itemTemplate)
+                @if($item->children->isNotEmpty())
+                    @include('components.tree', [
+                        'items' => $item->children,
+                        'itemTemplate' => $itemTemplate,
+                        'id' => $id,
+                        'parentItemId' => $item->id,
+                        'depth' => $depth + 1,
+                    ])
                 @endif
-            </div>
-
-            @if($item->children->isNotEmpty())
-                @include('components.tree', [
-                    'items' => $item->children,
-                    'itemTemplate' => $itemTemplate,
-                    'id' => $id,
-                    'parentItemId' => $item->id,
-                    'depth' => $depth + 1,
-                ])
-            @endif
-        </li>
-    @endforeach
-</ul>
+            </li>
+        @endforeach
+    </ul>
+@else
+    <div class="small text-secondary text-center mt-5">Нет данных</div>
+@endif
