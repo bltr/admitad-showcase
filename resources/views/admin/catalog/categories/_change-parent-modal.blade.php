fb2529b7abc0a8ad95ff92aaecd1d097fb5a5cd3
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2021-10-31 19:12:44 +0500

    wip

--- resources/views/admin/catalog/categories/_change-parent-modal.blade.php
+++ resources/views/admin/catalog/categories/_change-parent-modal.blade.php
@@ -2,13 +2,16 @@
 <div class="modal" id="choiceParentModal" tabindex="-1">
     <div class="modal-dialog  modal-lg modal-dialog-scrollable modal-dialog-centered">
         <div class="modal-content h-100">
+
             <div class="modal-header">
                 <h5 class="modal-title">Выбрать родительскую категорию</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+
             <div class="modal-body">
                 <form method="POST">
                     @csrf
+
                     <div class="d-flex">
                         <div class="ms-5">
                             <input
@@ -22,10 +25,7 @@
                         <div class="ms-2 me-auto ps-1 mb-2">Без родителя</div>
                     </div>
 
-                    @component('components.tree', [
-                        'items' => $categories,
-                        'id' => 'b'
-                    ])
+                    @component('components.tree', ['items' => $categories, 'id' => 'modal'])
                         @scopedSlot('itemTemplate', ($item))
                             <div class="me-2">
                                 <input
@@ -39,11 +39,14 @@
                             <div class="me-auto ms-1">{{ $item->name }}</div>
                         @endScopedSlot
                     @endcomponent
+
                 </form>
             </div>
+
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
             </div>
+
         </div>
     </div>
 </div>
